472939c74f98fa61ddb56d972bc98725cf22ae5c
S Romberg <SRomberg@Macbook-Pro.local>
2018-07-03 15:06:49 -0600

    updated site

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
         <div id='description' name='description'  className='title'>
             <h2>Full Stack Developer</h2>
-            <p >I am a full stack software developer currently expanding my skills <br/> and abilities at a Galvanize Immersive program. My experience in the work <br/> force has taught my to treat each obstacle as a new challenge to overcome, <br/> and to never be satisfied with the amount of understanding I have. In my <br/>free time I like to rock climb and go camping and hiking with my <br/> Australian Shepherd.</p>
+            <p >I am a full stack software developer currently expanding my skills <br/> and abilities at a Galvanize Immersive program. My experience in the work <br/> force has taught my to treat each obstacle as a new challenge to overcome, <br/> and to never be satisfied with the amount of understanding I have. In my <br/>free time I like to go camping and hiking with my Australian Shepherd.</p>
         </div>
 
     );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,34 +4,114 @@ import gdog from '../projects/G-dawgs.png'
 import inspo from '../projects/Inspogram.png'
 import island from '../projects/Island2you.png'
 import music from '../projects/MusicPlayland.png'
+import voter from '../projects/Voter.png'
+import link from '../projects/link.png'
 
 class Projects extends Component {
-  show(){
-    console.log('enter')
-  }
-  notShow(){
-    console.log('leave')
-  }
+
   render() {
     return (
       <div className='columnProjects'>
         <h2>Projects</h2>
         <div className='projects' >
           <div className='projectHold'>
-            <a href='https://galvanize-dogs.firebaseapp.com/'><img className='project' src={gdog} alt='gdog' onMouseEnter={this.show} onMouseLeave={this.notShow} /></a>
-            <a href='https://github.com/S-Romberg/Galvanize-Dogs'><img className='github' src={github} alt='github' /></a>
+          <div id='outer-box'>
+            <a href='https://galvanize-dogs.firebaseapp.com/'>
+              <img className='project' src={gdog} alt='gdog' />
+              <div id='inner-box'>
+              <img className='link' src={link} alt='link' />
+                <ul>
+                  <li>React.js</li>
+                  <li>JavaScript</li>
+                  <li>Knex.js</li>
+                  <li>Express</li>
+                  <li>PostgreSQL</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </a>
+          </div>
+            <a href='https://github.com/S-Romberg/Galvanize-Dogs'>
+              <img className='github' src={github} alt='github' />
+            </a>
           </div>
           <div className='projectHold'>
-            <a href='https://inspo-gram.firebaseapp.com' ><img className='project' src={inspo} alt='inspo' onMouseEnter={this.show} onMouseLeave={this.notShow} /></a>
-            <a href='https://github.com/S-Romberg/inspogram-client'><img className='github' src={github} alt='github' /></a>
+          <div id='outer-box'>
+            <a href='https://inspo-gram.firebaseapp.com' >
+              <img className='project' src={inspo} alt='inspo' />
+              <div id='inner-box'>
+              <img className='link' src={link} alt='link' />
+                  <ul>
+                  <li>React.js</li>
+                  <li>JavaScript</li>
+                  <li>Knex.js</li>
+                  <li>Express</li>
+                  <li>PostgreSQL</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </a>
+          </div>
+            <a href='https://github.com/S-Romberg/inspogram-client'>
+              <img className='github' src={github} alt='github' />
+            </a>
           </div>
           <div className='projectHold'>
-            <a href='https://islandtoyou.firebaseapp.com' ><img className='project' src={island} alt='island' onMouseEnter={this.show} onMouseLeave={this.notShow} /></a>
-            <a href='https://github.com/S-Romberg/islandLife'><img className='github' src={github} alt='github' /></a>
+          <div id='outer-box'>
+            <a href='https://voterappr.firebaseapp.com/'>
+              <img className='project' src={voter} alt='music' />
+              <div id='inner-box'>
+              <img className='link' src={link} alt='link' />
+                  <ul>
+                  <li>React.js</li>
+                  <li>JavaScript</li>
+                  <li>Knex.js</li>
+                  <li>Express</li>
+                  <li>PostgreSQL</li>
+                  <li>CSS</li>
+                  <li>OAuth</li>
+                </ul>
+              </div>
+            </a>
+          </div>
+            <a href='https://github.com/S-Romberg/voter'>
+              <img className='github' src={github} alt='github' />
+            </a>
           </div>
           <div className='projectHold'>
-            <a href='https://github.com/S-Romberg/music-playland'><img className='project' src={music} alt='music' onMouseEnter={this.show} onMouseLeave={this.notShow} /></a>
-            <a href='https://github.com/S-Romberg/music-playland'><img className='github' src={github} alt='github' /></a>
+          <div id='outer-box'>
+            <a href='https://islandtoyou.firebaseapp.com' >
+              <img className='project' src={island} alt='island' />
+              <div id='inner-box'>
+              <img className='link' src={link} alt='link' />
+                  <ul>
+                  <li>JavaScript</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </a>
+          </div>
+            <a href='https://github.com/S-Romberg/islandLife'>
+              <img className='github' src={github} alt='github' />
+            </a>
+          </div>
+          <div className='projectHold'>
+          <div id='outer-box'>
+            <a href='https://github.com/S-Romberg/music-playland'>
+              <img className='project' src={music} alt='music' />
+              <div id='inner-box'>
+                <img className='link' src={link} alt='link' />
+                <ul>
+                  <li>React.js</li>
+                  <li>JavaScript</li>
+                  <li>CSS</li>
+                </ul>
+              </div>
+            </a>
+          </div>
+            <a href='https://github.com/S-Romberg/music-playland'>
+              <img className='github' src={github} alt='github' />
+            </a>
           </div>
         </div>
       </div>
