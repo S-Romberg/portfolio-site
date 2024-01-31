a4f070798ebd7021e229ee0bea19776adcd0dbf6
Spencer Romberg <spencer.romberg@gmail.com>
2024-01-30 22:51:05 -0700

    added some descriptive text to projects, added external link icon

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
-import { Card, CardContent } from '@/components/ui/card';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLink } from '@fortawesome/free-solid-svg-icons';
 import {
   Carousel,
   CarouselContent,
@@ -8,16 +9,18 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/carousel';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default function Projects() {
   return (
     <div className="flex w-screen flex-col py-8 items-center justify-center">
       <h2 className="text-3xl text-slate-600 text-center pb-4">Projects</h2>
-      <Carousel className="w-full max-w-5xl">
-        <CarouselContent>
+      <Carousel className="w-fit max-w-5xl">
+        <CarouselContent className="flex items-center">
           <CarouselItem>
             <div className="p-1">
               <Card onClick={() => window.open('https://github.com/brewerdigital-llc/VivoCare')}>
+                <FontAwesomeIcon className="absolute right-4 top-4 text-slate-400" icon={faExternalLink} />
                 <CardContent className="flex flex-col gap-4 items-center justify-center p-6">
                   <Image
                     width={550}
@@ -25,7 +28,11 @@ export default function Projects() {
                     src="/vivocare.png"
                     alt="Photo of phones with the VivoCare app open"
                   />
-                  <span className="text-xl font-semibold">VivoCare</span>
+                  <span className="text-xl font-semibold text-slate-800">VivoCare</span>
+                  <span className="text-md text-slate-600 max-w-xl">
+                    An award-winning open-source mobile application.
+                    A React Native app developed by myself and one other developer in under 3 months.
+                  </span>
                 </CardContent>
               </Card>
             </div>
@@ -40,7 +47,8 @@ export default function Projects() {
                     src="/ocean-chart.png"
                     alt="Photo a global map plotting points with temperature readings"
                   />
-                  <span className="text-xl font-semibold">Predicting Coral Bleaching - Linear Regression Model</span>
+                  <span className="text-xl font-semibold text-slate-800">Predicting Coral Bleaching</span>
+                  <span className="text-md text-slate-600">A linear regression model trained using temperature readings to predict coral bleaching.</span>
                 </CardContent>
               </Card>
             </div>
